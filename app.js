06de322dac573173a2f2e47a0bad13d85da22ed9
federicofuria <federicofuriadev@gmail.com>
2023-05-03 13:18:38 -0300

    Creé todas las rutas de app.get para cada html

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use( express.static( path.join (__dirname, '/public')));
+app.use(express.static( path.join (__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get('/home-1', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home-1.html'))
+})
+
+app.get('/home-2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home-2.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'))
+})
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
 
 app.listen(3000, () => {
     console.log('Escuchando puerto 3000');
