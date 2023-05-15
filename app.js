e6eba4cfdef3ed33b2c6a502803baff82a182622
alemaccarthy <ale.maccarthy@gmail.com>
2023-05-14 23:47:06 -0300

    instalé ejs y creé header.ejs y footer.ejs

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static( path.join (__dirname, '/public')));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
