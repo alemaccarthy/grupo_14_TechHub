686dcc6653b7f1fdf8bf2fc824cf87c9b6a42a84
alemaccarthy <ale.maccarthy@gmail.com>
2023-05-26 16:35:07 -0300

    corregí un pequeño detalle del commit anterior

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -3,11 +3,12 @@ const productRoutes = express.Router();
 const productController = require('../controllers/productsController');
 
 // @Get /products
+productRoutes.get('/', productController.getProductsList);
 
+// @GET /products/detail
 productRoutes.get('/detail', productController.getProductDetail);
 
-productRoutes.get('/list', productController.getProductsList);
-
+// @GET /products/create
 productRoutes.get('/create', productController.getCreateProduct);
 
 module.exports = productRoutes;
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const usersRoutes = express.Router();
 const usersController = require('../controllers/usersController');
 
-// @ GET /user
-
+// @ GET /user/complete-purchase
 usersRoutes.get('/complete-purchase', usersController.getPurchase);
 
+// @GET /user/register
 usersRoutes.get('/register', usersController.getRegister);
 
 module.exports = usersRoutes;
