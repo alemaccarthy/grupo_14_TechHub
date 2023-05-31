8d292bdd04577c27423860d67a051eefd3d2ceaa
alemaccarthy <ale.maccarthy@gmail.com>
2023-05-31 18:24:23 -0300

    agregando rutas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -8,7 +8,10 @@ const productsController = {
         res.render('products-list');
     },
     getCreateProduct: (req, res) => {
-        res.render('create-product')
+        res.render('create-product');
+    },
+    getUpdateProduct: (req, res) => {
+        res.render('update-product');
     }
 }
 
