102b46e25adf5c1fcc8cd95dd2e3f818a945601a
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-06-10 01:09:27 -0300

    cambios

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const productRoutes = express.Router();
 const path = require('path');
-const { body } = require('express-validator');
 const multer = require('multer');
-
-const productController = require('../controllers/productsController');
-
+const {body} = require('express-validator');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/imgs/products-images');
+    destination: function(req, file, cb) {
+        cb(null, './public/imgs/products');
     },
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         let filename = `${Date.now()}_img${path.extname(file.originalname)}`;
         cb(null, filename);
     }
 });
 
-const upload = multer({ storage });
+const upload = multer({storage});
+
+const productController = require('../controllers/productsController');
 
 const validations = [
     body('title').notEmpty().withMessage('El titulo no puede estar vacio'),
@@ -26,24 +25,23 @@ const validations = [
     body('currency').notEmpty().withMessage('Debes elegir una moneda para expresar el precio del producto'),
     body('price').notEmpty().withMessage('Debes asignar un precio al producto'),
     body('category').notEmpty().withMessage('Debes seleccionar una categoria para el producto'),
-    body('colors').notEmpty().withMessage('Debes seleccionar la cantidad de colores disponibles'),
-    body('productImages').custom((value, { req }) => {
+    body('number').notEmpty().withMessage('Debes seleccionar la cantidad de colores disponibles'),
+    body('products-images').custom((value, {req}) => {
         let file = req.file;
-        // console.log(file);
         let acceptedExtensions = ['.jpg', '.png', ',jpeg'];
-        let fileExtension = path.extname(file.originalname);
-
-        if (!file) {
+        
+        if(!file){
             throw new Error('Debes subir una imagen del producto');
         }
-
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
-            }
+        else {
+            let fileExtension = path.extname(file.originalname);
+        }
+        if(!acceptedExtensions.includes(fileExtension)){
+            throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
+        }
         return true;
     })
 ]
-
 
 // @Get /products
 productRoutes.get('/', productController.getProducts);
@@ -55,7 +53,7 @@ productRoutes.get('/:id/detail', productController.getProductDetail);
 productRoutes.get('/create', productController.getCreateProduct);
 
 // @POST /products/create
-productRoutes.post('/create', [upload.any('products-images'), validations], productController.postProduct);
+productRoutes.post('/create', upload.any('products-images'), validations, productController.postProduct);
 
 // @DELETE /products/:id/delete
 
