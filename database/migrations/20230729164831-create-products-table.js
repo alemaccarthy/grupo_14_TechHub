e3b78710db3ad3e932e2e949d4c4067e32b7e795
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-07-29 14:20:37 -0300

    intentando migracion

---
+++ database/migrations/20230729164831-create-products-table.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      currency: {
+        type: Sequelize.STRING
+      },
+      images: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      deleted: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      brand_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'brands',
+        }
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+
+  }
+};
