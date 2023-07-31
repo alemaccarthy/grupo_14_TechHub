19d481a3b4e7a1c7f535ad81213c84f4887e400f
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-07-31 17:42:28 -0300

    agrego metodos al controlador de productos

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,4 +1,4 @@
-const { Product } = require('../database/models');
+const { Product, Color } = require('../database/models');
 const { validationResult } = require('express-validator');
 
 const productControllers = {
@@ -11,17 +11,41 @@ const productControllers = {
             currency: req.body.currency,
             category: req.body.category,
             images: req.body.images,
-            // FALTAN LOS COLORES !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            color_quantiy: req.body.color_quantiy,
         };
 
         try {
             const datos = await Product.create(newProduct);
+
+            const colors = [];
+            for (let i = 1; i <= req.body.color_quantity; i++) {
+                const colorField = `color${i}`;
+                const colorValue = req.body[colorField];
+                colors.push({ color: colorValue, product_id: Product.id });
+            }
+            await Color.bulkCreate(colors);
+
             console.log(datos);
         } catch (error) {
             console.log(error);
         }
 
-        res.render('products-list', { title: '| Productos'}) //?????????????
+        res.render('products-list', { title: '| Productos' }) //?????????????
+    },
+
+    getUpdateProduct: async (req, res) => {
+        try {
+            const updatedProduct = await Product.findByPk(req.params.id);
+            if (!updatedProduct) {
+                // Con el return detenemos la ejecución del controller, y con el res.send enviamos un mensaje de error
+                // *queremos detener la ejecución para que no se ejecute el otro res.render (la otra respuesta)
+                return res.render('product-not-found');
+            }
+
+            res.render('update-product', { updatedProduct })
+        } catch (error) {
+            console.log(error);
+        }
     },
 
     updateProduct: async (req, res) => {
@@ -39,7 +63,25 @@ const productControllers = {
             res.send("no se pudo actualizar");
             console.log(error);
         }
+    },
+
+    deleteProduct: async (req, res) => {
+        try {
+            const product = await Product.findByPk(req.params.id);
+            if (!product) {
+                return res.status(404).send('No se encontro el producto');
+            }
+            else {
+                await product.update({
+                    deleted: true
+                });
+                res.redirect('/products');
+            }
+        }
+        catch (error) {
+            res.status(500).send('No se pudo eliminar el producto');
+        }
     }
-};
+}
 
 module.exports = productControllers;
