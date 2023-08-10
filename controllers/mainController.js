fb4e7d14f58b2702aa76ba516314cb79d58921d9
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-08-09 22:24:21 -0300

    cambios

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,25 +1,62 @@
 const path = require('path');
+const { Product, Color, Category, Brand, Image } = require('../database/models');
 
 const mainController = {
     getIndex: (req, res) => {
-        res.render('index', { title: 'TechHub'});
+        res.render('index', { title: 'TechHub' });
     },
 
-    getHome1: (req, res) => {
-        const selectedBrandraw = req.cookies.selectedBrand;
-        const selectedBrand = selectedBrandraw.charAt(0).toUpperCase() + selectedBrandraw.slice(1);
-        res.render('apple', { title: 'TechHub', selectedBrand})
+    getHome1: async (req, res) => {
+        try {
+            const selectedBrandraw = req.cookies.selectedBrand;
+            const selectedBrand = selectedBrandraw.charAt(0).toUpperCase() + selectedBrandraw.slice(1);
+
+            // Consulta a la base de datos para obtener la categoría de productos de la marca seleccionada
+            const products = await Product.findAll({
+                include: [
+                    { model: Brand, as: 'brand' },
+                    { model: Category, as: 'category' }
+                ],
+                where: {
+                    '$brand.name$': selectedBrand,
+                    deletedAt: null // Si deseas filtrar productos eliminados
+                }
+            });
+            console.log('ESTOS SON LOS PRODUCTOS EN HOME APPLE ' + JSON.stringify(products, null, 2)); //ASI LOS PODES MOSTRAR POR CONSOLA
+            res.render('apple', { title: 'TechHub', selectedBrand, products });
+        } catch (error) {
+            res.render('apple', { title: 'TechHub', category: null });
+        }
     },
 
-    getHome2: (req, res) => {      
-        const selectedBrandraw = req.cookies.selectedBrand;
-        const selectedBrand = selectedBrandraw.charAt(0).toUpperCase() + selectedBrandraw.slice(1);
-        res.render('samsung', { title: 'TechHub', selectedBrand})
+    getHome2: async (req, res) => {
+        try {
+            const selectedBrandraw = req.cookies.selectedBrand;
+            const selectedBrand = selectedBrandraw.charAt(0).toUpperCase() + selectedBrandraw.slice(1);
+
+            // Consulta a la base de datos para obtener la categoría de productos de la marca seleccionada
+            const products = await Product.findAll({
+                include: [
+                    { model: Brand, as: 'brand' },
+                    { model: Category, as: 'category' }
+                ],
+                where: {
+                    '$brand.name$': selectedBrand,
+                    deletedAt: null // Si deseas filtrar productos eliminados
+                }
+            });
+
+            
+            res.render('samsung', { title: 'TechHub', selectedBrand });
+        } catch (error) {
+            res.render('samsung', { title: 'TechHub', selectedBrand });
+        }
     },
+    
     getFaq: (req, res) => {
         const selectedBrandraw = req.cookies.selectedBrand;
         const selectedBrand = selectedBrandraw.charAt(0).toUpperCase() + selectedBrandraw.slice(1);
-        res.render('faq', { title: 'Preguntas frecuentes', selectedBrand})
+        res.render('faq', { title: 'Preguntas frecuentes', selectedBrand })
     }
 }
 
