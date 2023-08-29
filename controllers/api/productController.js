70035b9445413aef0ea054eeb2a8af2e53e89ebd
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-08-29 01:16:19 -0300

    dejo terminada la api de products

--- controllers/api/productController.js
+++ controllers/api/productController.js
@@ -1,31 +1,77 @@
 const { Product, Brand, Color, Category, Image } = require('../../database/models');
-const { Op } = require('sequelize');
+const { Op, Sequelize } = require('sequelize');
+
+
 
 module.exports = {
     getAll: async (req, res) => {
-        const products = await Product.findAll({
-            raw: true,
-            include: [
-                { model: Brand, as: 'brand' },
-                { model: Category, as: 'category' },
-                { model: Image, as: 'images' },
-            ],
-            nest: true,
-            where: {
-                deletedAt: {
-                    [Op.eq]: null 
+        try {
+            // Consulta para obtener la cantidad total de productos
+            const totalProducts = await Product.count({
+                where: {
+                    deletedAt: {
+                        [Op.eq]: null
+                    }
                 }
-            },
-        });
+            });
     
-        products.forEach(product => {
-            product.productDetail = {
-                url: `http://localhost:3000/api/products/${product.id}/detail`
+            // Consulta para obtener la cantidad de productos por categoría
+            const productsByCategory = await Category.findAll({
+                attributes: ['name', [Sequelize.fn('COUNT', Sequelize.col('products_category.id')), 'productCount']],
+                include: [{
+                    model: Product,
+                    as: 'products_category',
+                    attributes: [],
+                    where: {
+                        deletedAt: {
+                            [Op.eq]: null
+                        }
+                    },
+                }],
+                group: ['Category.id', 'Category.name'],
+            });
+    
+            // Consulta para obtener la lista de productos
+            const products = await Product.findAll({
+                raw: true,
+                include: [
+                    { model: Brand, as: 'brand' },
+                    { model: Category, as: 'category' },
+                    { model: Image, as: 'images' },
+                ],
+                nest: true,
+                where: {
+                    deletedAt: {
+                        [Op.eq]: null
+                    }
+                },
+            });
+    
+            // Agrego la información adicional a cada producto
+            products.forEach(product => {
+                product.productDetail = {
+                    url: `http://localhost:3000/api/products/${product.id}/detail`
+                }
+            });
+    
+            // Creo un objeto para almacenar la información final
+            const response = {
+                totalProducts: totalProducts,
+                productsByCategory: {},
+                products: products,
             };
-        });
+
+            productsByCategory.forEach(category => {
+                response.productsByCategory[category.name] = category.get('productCount');
+            });
     
-        res.json(products);
+            res.json(response);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al obtener los productos.' });
+        }
     },
+    
     
 
     getProductDetail: async (req, res) => {
@@ -37,18 +83,18 @@ module.exports = {
                 { model: Image, as: 'images' },
                 {
                     model: Color,
-                    through: { attributes: [] }, // Para excluir las columnas de la tabla intermedia
+                    through: { attributes: [] },
                     as: 'colors'
                 }
             ],
             where: {
                 deletedAt: {
-                    [Op.eq]: null // Filtra productos que no se les aplico soft Delete
+                    [Op.eq]: null
                 },
 
             }
         });
+
         res.json(product);
     },
- 
 }
