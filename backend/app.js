753909c2099060ebeaa9134a392eec976a813486
Ale Mac Carthy <ale.maccarthy@gmail.com>
2023-09-11 01:48:14 -0300

    bla

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1 * 60 * 1000,
+        maxAge: 60 * 60 * 1000,
         httpOnly: true,
         sameSite: 'lax',
     },
