f93ba81d06320562775e80248f45880b3836c68c
Marto85 <104855025+Marto85@users.noreply.github.com>
2023-09-12 00:15:17 -0300

    visualiza imagen ultimo producto

--- frontend/src/components/LastProduct.js
+++ frontend/src/components/LastProduct.js
@@ -4,6 +4,7 @@ function LastProduct({ products }) {
     if (products.length === 0) {
         return null;
     }
+    const url_base = 'http://localhost:3000'
 
     const lastProduct = products[0];
 
@@ -11,15 +12,15 @@ function LastProduct({ products }) {
         <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h4 className="m-0 font-weight-bold text-gray-800">Last product in Data Base</h4>
+                    <h4 className="m-0 font-weight-bold text-gray-800">Last product in Database</h4>
                 </div>
                 <div className="card-body">
                     <div className="text-center">
-
+                        <h4>{lastProduct.title}</h4>
                         <img
                             className="img-fluid px-3 px-sm-4 mt-3 mb-4"
                             style={{ width: '40rem' }}
-                            src={lastProduct.images.length > 0 ? lastProduct.images[0].path : ''}
+                            src={lastProduct.images.length > 0 ? url_base + lastProduct.images[0].path : ''}
                             alt={lastProduct.title}
                         />
                     </div>
