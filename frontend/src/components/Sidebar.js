56061dc48643d51a0e2cc548dccb057ee9de2b1a
Ale Mac Carthy <ale.maccarthy@gmail.com>
2023-09-27 00:03:01 -0300

    estilos dashboard

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -3,28 +3,13 @@ import logoTechHub from '../assets/images/LogoTechHub_Blanca.png';
 
 function Sidebar() {
     return (
-        <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul className="navbar-nav" id="accordionSidebar">
             {/* Sidebar - Brand */}
-            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+            <a className="sidebar-brand d-flex align-items-center justify-content-start" href="/">
                 <div className="sidebar-brand-icon">
-                    <img className="w-100" src={logoTechHub} alt="TechHub" />
+                    <img className="logo" src={logoTechHub} alt="TechHub" />
                 </div>
             </a>
-
-            {/* Divider */}
-            <hr className="sidebar-divider my-0" />
-
-            {/* Nav Item - Dashboard */}
-            <li className="nav-item active">
-                <a className="nav-link" href="/">
-                    <i className="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard - TechHub</span>
-                </a>
-            </li>
-
-            {/* Divider */}
-            <hr className="sidebar-divider" />
-
         </ul>
     );
 }
